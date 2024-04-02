acd4e8eeefe472eb07f06928df870e8dc404bded
Ilkka Kuosmanen <ilkka.kuosmanen@lightningchart.com>
2024-04-02 12:54:51 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ const {
 // Create 3D chart
 const chart = lightningChart()
     .Chart3D({
-        theme: Themes[new URLSearchParams(window.location.search).get('theme') || 'darkGold'] || undefined
+        theme: Themes[new URLSearchParams(window.location.search).get('theme') || 'darkGold'] || undefined,
     })
     .setTitle(`LiDAR Point Cloud`)
 
